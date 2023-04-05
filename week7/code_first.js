178f3505c224446e18902bdc24da52539fb71fdd
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-05 19:54:10 +0300

    Update and rename code_first.js to code_first.js

--- week7/code_first.js
+++ week7/code_first.js
@@ -1,4 +1,6 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import * as THREE from '../three/three.module.js';
+
+// Our Javascript will go here.
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -16,19 +18,19 @@ const cylgeometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
 const cylmaterial = new THREE.MeshLambertMaterial( {color: 0xffff00} );
 const cylinder = new THREE.Mesh( cylgeometry, cylmaterial );
 scene.add( cylinder );
-cylinder.position.z =-25;
-cylinder.position.x = 5;
+cylinder.position.z=-25;
+cylinder.position.x=5;
 
-var lightOne=new THREE.AmbientLight(0xffffff, 0.5); 
-scene.add(lightOne); 
+var lightOne=new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(lightOne);
 
-var lightTwo=new THREE.PointLight(0xffffff, 0.5); 
-scene.add(lightTwo); 
+var lightTwo=new THREE.PointLight(0xffffff, 0.5);
+scene.add(lightTwo);
 
-lightTwo.position.set(25, 0, -25 )
+lightTwo.position.set(25, 0, -25)
 
 camera.position.z = 7;
-camera.position.x = 5;
+camera.position.x = 2;
 
 renderer.setClearColor (0x555555);
 renderer.clear();
@@ -40,13 +42,13 @@ function animate() {
 	renderer.render( scene, camera );
 	lightTwo.position.x = radius * Math.cos(angle) + 5;
 	lightTwo.position.y = radius * Math.sin(angle);
-
-/*	cube.rotation.x += 0.01;
+/*
+	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	camera.position.x = radius * Math.cos(angle) + 2;
-	camera.position.y = radius * Math.sin(angle);	
+	camera.position.y = radius * Math.sin(angle);
 */
-	angle += Math.PI/180;	
+	angle += Math.PI/180;
 }
 
 animate();

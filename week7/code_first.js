12e6ab31ae54bd7c238d5a990879f338461ed1b8
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-05 15:44:47 +0300

    Update code_first.js

--- week7/code_first.js
+++ week7/code_first.js
@@ -15,3 +15,9 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+function animate() {
+	       requestAnimationFrame( animate );
+	       renderer.render( scene, camera );
+}
+animate();

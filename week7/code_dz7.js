4a515ffa28391b6e4411eeb8cdb96b7fa6048f54
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-06 19:46:26 +0300

    Update code_dz7.js

--- week7/code_dz7.js
+++ week7/code_dz7.js
@@ -68,7 +68,7 @@ const planemesh=new THREE.Mesh(planegeometry, planematerial);
 planemesh.position.set(0, -20, -100); 
 scene.add(planemesh);
 
-var delta = 0;
+/*var delta = 0;*/
 
 var lightOne=new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(lightOne);
@@ -78,9 +78,9 @@ scene.add(lightTwo);
 
 
 function animate() {
-	delta += 0.1; 
+	/*delta += 0.1; 
 	planegeometry.vertices[0].z = -25 + Math.sin(delta)*50; 
-	planegeometry.verticesNeedUpdate = true;
+	planegeometry.verticesNeedUpdate = true;*/
 	
         pyramidmesh.rotation.y+=0.1; 
 	

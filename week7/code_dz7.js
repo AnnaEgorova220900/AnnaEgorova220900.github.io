e30f35a3920e94e60f1c14888d19ab282b9f4eed
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-06 07:09:53 +0300

    Update code_dz7.js

--- week7/code_dz7.js
+++ week7/code_dz7.js
@@ -17,6 +17,25 @@ const pyramidmesh=new THREE.Mesh(pyramidgeometry, pyramidmaterial);
 pyramidmesh.position.set(0, 2, -10); 
 scene.add(pyramidmesh); 
 
+const boxgeometry=new THREE.BoxGeometry(1, 1, 1); 
+const boxmaterial=new THREE.MeshBasicMaterial(); 
+const boxmesh=new THREE.Mesh(boxgeometry, boxmaterial); 
+boxmesh.position.set(-0.9, 0, -6); 
+scene.add(boxmesh);
+
+const spheregeometry=new THREE.SphereGeometry(0.5); 
+const spherematerial=new THREE.MeshBasicMaterial({wireframe: true, color: 0x0000000}); 
+const spheremesh=new THREE.Mesh(spheregeometry, spherematerial); 
+spheremesh.position.set(0.9, 0, -6); 
+scene.add(spheremesh); 
+
+const circlegeometry=new THREE.CircleBufferGeometry(0.5); 
+const circlematerial=new THREE.MeshBasicMaterial( {color: 0x0000000}); 
+const circlemesh=new THREE.Mesh(circlegeometry, circlematerial); 
+circlemesh.position.set(2, 0, -6); 
+circlemesh.rotation.set(0, 0.5, 0); 
+scene.add(circlemesh); 
+
 var lightOne=new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(lightOne);
 

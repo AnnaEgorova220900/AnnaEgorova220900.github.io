3fd6dffc5c446b8306b5a2b5f8b4ae618ccfac7f
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-06 21:18:13 +0300

    Update code_dz7.js

--- week7/code_dz7.js
+++ week7/code_dz7.js
@@ -79,7 +79,7 @@ scene.add(lightTwo);
 
 function animate() {
 	delta += 0.1; 
-	planegeometry.vertices[i].z = -25 + Math.sin(delta)*50; 
+	planegeometry.vertices[0].z = -25 + Math.sin(delta)*50; 
 	planegeometry.verticesNeedUpdate = true;
 	
         pyramidmesh.rotation.y+=0.1; 

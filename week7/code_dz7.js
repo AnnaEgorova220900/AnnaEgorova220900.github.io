326e77d1075c16c88292d72717454c0194379f30
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-06 14:34:38 +0300

    Update code_dz7.js

--- week7/code_dz7.js
+++ week7/code_dz7.js
@@ -44,7 +44,7 @@ circlemesh.position.set(2, 0, -6);
 circlemesh.rotation.set(0, 0.5, 0); 
 scene.add(circlemesh); 
 
-/*var paraFunction=function(a, b) 
+var paraFunction=function(a, b) 
 { 
 	var x=-5+5*a; 
 	var y=-5+5*b; 
@@ -56,7 +56,7 @@ const parageometry= new THREE.ParametricGeometry(paraFunction, 8, 8);
 const paramaterial= new THREE.MeshBasicMaterial( {color: 0xF3FFE2});
 const paramesh= new THREE.Mesh(parageometry, paramaterial);
 paramesh.position.set(0, -2, -100);
-scene.add(paramesh);*/
+scene.add(paramesh);
 
 const planegeometry=new THREE.PlaneGeometry(10, 10); 
 const planematerial=new THREE.MeshPhongMaterial({ 
@@ -83,12 +83,12 @@ function animate() {
 	planegeometry.verticesNeedUpdate = true;
 	
         pyramidmesh.rotation.y+=0.1; 
-	/*paramesh.rotation.x+=0.1; 
-	paramesh.rotation.y+=0.1;*/
-
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
 	
+	paramesh.rotation.x+=0.1; 
+	paramesh.rotation.y+=0.1;
+
+	renderer.render( scene, camera );
+	requestAnimationFrame( animate );	
 }
 
 animate();

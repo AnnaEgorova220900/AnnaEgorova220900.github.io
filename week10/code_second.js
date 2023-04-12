e7008b186d765a99dd0daf714d5d7b4efe9a4adc
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-04-12 09:00:15 +0300

    Update code_second.js

--- week10/code_second.js
+++ week10/code_second.js
@@ -19,8 +19,8 @@ const boxmaterials = [
 		new THREE.MeshBasicMaterial( { color: Math.random()*0xffffff } )
 	];
 
-cube = new THREE.Mesh( boxgeometry, boxmaterials );
-scene.add( cube );
+const boxmesh=new THREE.Mesh(boxgeometry, boxmaterials); 
+scene.add(boxmesh);
 
 const cylgeometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
 const cylmaterial = new THREE.MeshLambertMaterial( {color: 0xffff00} );

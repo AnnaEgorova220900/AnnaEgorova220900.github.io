babb896ab386a13e31f1e5fcd4786dc6955daf93
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-05-06 23:50:26 +0300

    Update tm_exp2.js

--- week12/tm_exp2.js
+++ week12/tm_exp2.js
@@ -34,58 +34,54 @@ async function loop() {
 const skipCount = 5;
 let frameCount = 0;
 
-// run the webcam image through the image model
-async function predict() {
-	if(frameCount % skipCount == 0)
-	{
-		const hands = await detector.estimateHands(webcam.canvas);
-		//console.log(hands);
-
-		if(hands.length == 0)
-			labelContainer.innerHTML = "Не бачу рук";
-		else
-		{
-			const hand = hands[0];
-			const landmarks = hand.landmarks;
-
-			// calculate finger lengths
-			const fingerLengths = [				distance(landmarks[5], landmarks[9]), // thumb
-				distance(landmarks[9], landmarks[13]), // index
-				distance(landmarks[13], landmarks[17]), // middle
-				distance(landmarks[17], landmarks[21]), // ring
-				distance(landmarks[21], landmarks[25]) // pinky
-			];
-
-			// calculate angles between fingers
-			const angles = [				angle(landmarks[0], landmarks[1], landmarks[2]), // thumb
-				angle(landmarks[5], landmarks[6], landmarks[7]), // index
-				angle(landmarks[9], landmarks[10], landmarks[11]),// middle
-        distance(landmarks[14], landmarks[15]), // ring
-        distance(landmarks[18], landmarks[19]) // pinky
-    ];
-
-    // output angles to label container
-    let label = "";
-    for (let i = 0; i < angles.length; i++) {
-        label += "Angle " + (i + 1) + ": " + angles[i].toFixed(2) + " degrees<br>";
-    }
-    labelContainer.innerHTML = label;
-}
 function distance(a, b) {
     const xDiff = a.x - b.x;
     const yDiff = a.y - b.y;
     return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 }
 
-function angle(a, b, c) {
-    const radians = Math.atan2(c.y - b.y, c.x - b.x) - Math.atan2(a.y - b.y, a.x - b.x);
-    let degrees = radians * 180 / Math.PI;
-    if (degrees < 0) {
-        degrees += 360;
+
+// run the webcam image through the image model
+async function predict() {
+  if(frameCount % skipCount == 0)
+  {
+    const hands = await detector.estimateHands(webcam.canvas);
+
+    if (hands && hands.length > 0) {
+      const hand = hands[0];
+      const landmarks = hand.landmarks;
+
+      if (landmarks) {
+        // calculate finger lengths
+        const fingerLengths = [          distance(landmarks[5], landmarks[9]), // thumb
+          distance(landmarks[9], landmarks[13]), // index
+          distance(landmarks[13], landmarks[17]), // middle
+          distance(landmarks[17], landmarks[21]), // ring
+          distance(landmarks[21], landmarks[25]) // pinky
+        ];
+
+        // calculate angles between fingers
+        const angles = [          angle(landmarks[0], landmarks[1], landmarks[2]), // thumb
+          angle(landmarks[5], landmarks[6], landmarks[7]), // index
+          angle(landmarks[9], landmarks[10], landmarks[11]),// middle
+          distance(landmarks[14], landmarks[15]), // ring
+          distance(landmarks[18], landmarks[19]) // pinky
+        ];
+
+        // output angles to label container
+        let label = "";
+        for (let i = 0; i < angles.length; i++) {
+          label += "Angle " + (i + 1) + ": " + angles[i].toFixed(2) + " degrees<br>";
+        }
+        labelContainer.innerHTML = label;
+      } else {
+        labelContainer.innerHTML = "Не знайдено ключових точок рук";
+      }
+    } else {
+      labelContainer.innerHTML = "Не бачу рук";
     }
-    return degrees;
+  }
+  frameCount++;
 }
 
-}
-}
 

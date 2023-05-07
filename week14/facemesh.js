69c12f584b76e4a428b315469c0a618ffcfacfd5
AnnaEgorova220900 <124664179+AnnaEgorova220900@users.noreply.github.com>
2023-05-07 23:48:49 +0300

    Update facemesh.js

--- week14/facemesh.js
+++ week14/facemesh.js
@@ -12,14 +12,11 @@ window.addEventListener("DOMContentLoaded", async () => {
 
       const anchor2 = mindarThree.addAnchor(10);
 
-	var lightOne=new THREE.AmbientLight(0xffffff, 1);
+      var lightOne=new THREE.AmbientLight(0xffffff, 1);
 	scene.add(lightOne);
 
-	const light = new THREE.HemisphereLight( 0xffffbb, 0xcccccc, 1 );
+      const light = new THREE.HemisphereLight( 0xffffbb, 0xcccccc, 1 );
 	scene.add( light );
-
-	
-
 
       const texture = new THREE.TextureLoader().load('1_kqi9JK434G5M3UYKfX_BbA-removebg-preview.png');
 
@@ -27,9 +24,7 @@ window.addEventListener("DOMContentLoaded", async () => {
       faceMesh.material.map = texture;
       faceMesh.material.transparent = true;
       faceMesh.material.needsUpdate = true;
-      //faceMesh.material.wireframe=true;
       scene.add(faceMesh);
-
 
       await mindarThree.start();
 
